c790f013f7152a941671770b212566417fb2506e
Lucas Trentim <lu.trentim@gmail.com>
2020-05-11 23:56:25 -0700

    Adds basic game infrastructure
    This change adds a basic terrain generator, a controllable player character and animations.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import {MainScene} from "./scenes/MainScene";
+
 
 const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
+    type: Phaser.AUTO,
+    scaleMode: Phaser.Scale.FIT,
+    pixelArt: true,
+    width: 336,
+    height: 336,
+    parent: "phaser-example",
+    scene: [MainScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 }
+        }
+    }
 };
 
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+new Phaser.Game(config);
